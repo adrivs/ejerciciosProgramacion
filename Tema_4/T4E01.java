8e1cda654cb9f6bd38993392304fbf101363b090
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2017-10-17 10:43:00 +0200

    Ejercicios tema 4 (no todos)

---
+++ Tema_4/T4E01.java
@@ -0,0 +1,32 @@
+public class T4E01 {
+	
+	public static void main (String[] args) {
+		System.out.print("Introduce un día de la semana (en minúsculas por favor): ");
+    String día = System.console().readLine();
+    
+    switch (día) {
+      case "lunes":
+      System.out.print("A primera hora hay Sistemas Informáticos");
+      break;
+      
+      case "martes":
+      System.out.print("A primera hora hay Programación");
+      break;
+      
+      case "miercoles":
+      System.out.print("A primera hora hay Sistemas Informáticos");
+      break;
+      
+      case "jueves":
+      System.out.print("A primera hora hay Programación");
+      break;
+      
+      case "viernes":
+      System.out.print("A primera hora hay Programación");
+      break;
+      
+      default:
+    
+}
+}
+}
