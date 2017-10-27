141d800e8557f588c1fe0b8f805f24efc61a1416
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2017-10-27 09:09:58 +0200

    Tema 5

--- Tema_5/T5EJ03.java
+++ Tema_5/T5EJ03.java
@@ -2,6 +2,13 @@ public class T5EJ03 {
 	
 	public static void main (String[] args) {
 		
+    int i = 0;
+    
+    do {
+      System.out.println(i*5);
+      i++;
+      } while (i<21);
+    
 	}
 }
 
