ee9e6c1b17f793eaa5c87b0abf59e75d69da0c3e
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2017-10-27 08:28:32 +0200

    Tema 5

---
+++ Tema_5/T5EJ01.java
@@ -0,0 +1,13 @@
+public class T5EJ01 {
+	
+	public static void main (String[] args) {
+		
+    for (int i = 0 ; i <= 100; i++) {
+      
+      if (i % 5 == 0) {
+         System.out.println (i);
+      }
+    }
+	}
+}
+
---
+++ Tema_5/T5EJ02.java
@@ -0,0 +1,8 @@
+
+public class T5EJ02 {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
