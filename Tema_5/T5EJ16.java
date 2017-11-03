cccd8d8da8a705588643533439b42b20938e33da
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2017-11-03 09:51:29 +0100

    Añadiendo ejs Tema5

--- Tema_5/T5EJ16.java
+++ Tema_5/T5EJ16.java
@@ -5,27 +5,37 @@ public class T5EJ16 {
 	public static void main (String[] args) {
 		
     int aux = 0;
-    int aux2 = 0;
-    
-    
+      
     Scanner s = new Scanner (System.in);
     
-    System.out.print("Introduc un número y te diré si es primo o no: ");
+    System.out.print("Introduce un número y te diré si es primo o no: ");
     int n = Integer.parseInt(s.nextLine());
     
-    for (int i = 0; i < n; i++) {
+    if (n == 2) {
       
+      System.out.print(n + " es primo.");
       
+      } else {
+        
+        for (int i = 1; i < n+1; i++) {
       
-      
-      
-      
-      
-      }
-    
-    
-    
-    
+      aux = n%i;
+                
+      } if (aux == 0 && n%2 !=0) {
+        
+        System.out.print(n + " es primo.");
+        
+        } else {
+          
+          System.out.print(n + " no es primo.");
+          
+          }
+        
+        
+        
+        
+        
+        }
     
     
 	}
