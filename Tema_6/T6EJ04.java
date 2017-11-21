943b693a7163b7615c02f4255f97052b12b72ec5
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2017-11-21 09:26:42 +0100

    Ejs T6

---
+++ Tema_6/T6EJ04.java
@@ -0,0 +1,15 @@
+public class T6EJ04 {
+	
+	public static void main (String[] args) {
+		
+    
+    
+    for (int i = 0; i < 20; i++) {
+      int numero = ((int)(Math.random()*11));
+      System.out.print(numero + " ");
+      
+      }
+    
+	}
+}
+
