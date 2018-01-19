f4ea3a5bd900b1cb9e48c29acbdc98a318011261
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-01-19 09:07:17 +0100

    Añadiendo ejercicios

---
+++ Tema_7/Arrays Bidimensionales/T8EJ23.java
@@ -0,0 +1,43 @@
+// EJ 6 ARRAYS BIDIMENSIONALES
+public class T8EJ23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [][] array = new int [6][10];
+		
+		int max = 0;
+		int min = 9999;
+		int aux = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			for (int j = 0; j < array[i].length; j++) {
+				 
+				array[i][j] = (int)(Math.random()*1001);
+				
+				if (array[i][j] == aux) {
+					
+					array[i][j] = (int)(Math.random()*1001);
+					aux = array[i][j];
+				
+				}
+				
+				if (array[i][j] > max){
+			        max = array[i][j];
+			      } else if (array[i][j] < min){
+			        min = array[i][j];
+			      }
+				
+				System.out.println(array[i][j]);
+				
+			}
+			
+		}
+		
+		
+		System.out.println("Mínimo: " + min);
+		System.out.println("Máximo: " + max);
+	}
+
+}
